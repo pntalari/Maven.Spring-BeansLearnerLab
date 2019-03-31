571ebfb426910b8af404c355a12dc3fd30a4f1f5
Nirmala <nirmala.talari@gmail.com>
2019-03-31 15:42:53 -0400

    TestsWIP

---
+++ src/main/java/com/zipcodewilmington/springbeanslearnerslab/LearnerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zipcodewilmington.springbeanslearnerslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerSpringApplication.class, args);
+	}
+
+}
