b4fec171e4b3de6709239f92f2dcdf5a6445c80c
Nirmala <nirmala.talari@gmail.com>
2019-03-31 17:55:13 -0400

    ClassroomConfigTestsWIP

--- src/test/java/com/zipcodewilmington/springbeanslearnerslab/config/ClassroomConfigTest.java
+++ src/test/java/com/zipcodewilmington/springbeanslearnerslab/config/ClassroomConfigTest.java
@@ -1,10 +1,18 @@
 package com.zipcodewilmington.springbeanslearnerslab.config;
 
+import com.zipcodewilmington.springbeanslearnerslab.models.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,11 +21,46 @@ import static org.junit.Assert.*;
 @ContextConfiguration
 public class ClassroomConfigTest {
 
+    @Autowired
+    @Qualifier("currCohort")
+    Classroom currentCohort;
+
+    @Autowired
+    @Qualifier("instructors")
+    Instructors instructors;
+
+    @Autowired
+    @Qualifier("currentStudents")
+    Students currentStuds;
+
+    @Autowired
+    @Qualifier("prevCohort")
+    Classroom previousCohort;
+
+    @Autowired
+    @Qualifier("previousStudents")
+    Students previousStuds;
+
+
     @Test
-    public void currentCohort() {
+    public void currentCohortTest() {
+
+        Students expCurrStuds = currentStuds;
+
+        List<Students> actCurrStuds = currentCohort.getStudentsList();
+
+        Assert.assertEquals(expCurrStuds, actCurrStuds);
+
     }
 
     @Test
-    public void previousCohort() {
+    public void previousCohortTest() {
+        Students expPrevStuds = previousStuds;
+
+        List<Students> actPrevStuds = previousCohort.getStudentsList();
+
+        Assert.assertEquals(expPrevStuds, actPrevStuds);
+
+
     }
 }
