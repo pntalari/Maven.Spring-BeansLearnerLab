6b13e130c5df0e90a55df7a9f2f48beff0d540c4
Nirmala <nirmala.talari@gmail.com>
2019-03-31 16:05:57 -0400

    StudetConfigTestsDone

--- src/test/java/com.zipcodewilmington.springbeanslearnerslab/configTests/StudentConfigTest.java
+++ src/test/java/com.zipcodewilmington.springbeanslearnerslab/configTests/StudentConfigTest.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.springbeanslearnerslab.configTests;
 import com.zipcodewilmington.springbeanslearnerslab.config.StudentConfig;
 import com.zipcodewilmington.springbeanslearnerslab.models.Student;
 import com.zipcodewilmington.springbeanslearnerslab.models.Students;
+import net.bytebuddy.implementation.bytecode.assign.TypeCasting;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,21 +27,43 @@ public class StudentConfigTest {
     @Autowired @Qualifier ("currentStudents")
     Students currentStudents;
 
+    @Autowired @Qualifier ("previousStudents")
+    Students previousStudents;
+
     @Test
-    public void currentStudentsSize() {
+    public void StudentsSize() {
+        int expectedPrevSize = 30;
+        int expectedCurrentSize = 26;
+        Assert.assertEquals(expectedCurrentSize,currentStudents.getPeopleList().size());
+        Assert.assertEquals(expectedPrevSize,previousStudents.getPeopleList().size());
+    }
 
-        int expectedSize = 26;
-        Assert.assertEquals(expectedSize,currentStudents.getPeopleList().size());
 
+    @Test
+    public void currentStudentsTest() {
+        List<String> expectedCurrStudNames = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F",
+                "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+                "V", "W", "X", "Y", "Z"));
+     //   List<String> expectedCurrStudsList = new ArrayList<>(expectedCurrStudNames);
+
+        List<Student> actualCurrStudNames = currentStudents.getPeopleList();
+
+        actualCurrStudNames.forEach(student -> Assert.assertTrue(expectedCurrStudNames.contains(student.getName())));
+
+        //Assert.assertTrue(actualCurrStudNames.);
     }
 
     @Test
     public void previousStudentsTest() {
-        List<String> expectedCurrStudNames = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F",
-                "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-                "V", "W", "X", "Y", "Z"));
-        List<Student> expectedCurrStudsList = new ArrayList<>(26);
+        List<String> expectedPrevStudNames = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6",
+                "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21",
+                "22", "23", "24", "25", "26", "27", "28", "29", "30"));
 
+        List<Student> actualPrevStudNames = previousStudents.getPeopleList();
+
+        actualPrevStudNames.forEach(student -> Assert.assertTrue(expectedPrevStudNames.contains(student.getName())));
 
     }
+
+
 }
